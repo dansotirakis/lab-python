1fee09c1684ee3bee91983164624e8ea3f39ade3
Damianos Sotirakis <dansotirakis18@gmail.com>
2021-04-01 01:46:55 -0300

    feat(dictionary) : adding an item, nested, theory

---
+++ Topics/Dictionary/Nested/main.py
@@ -0,0 +1,6 @@
+# please work with the variable children
+children = {
+    'Emily': {'profession': 'artist', 'age': int(5)},
+    'Adam': {'profession': 'astronaut', 'age': int(9)},
+    'Nancy': {'profession': 'programmer', 'age': int(14)}
+}
